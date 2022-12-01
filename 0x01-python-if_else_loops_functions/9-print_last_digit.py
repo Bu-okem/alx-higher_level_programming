3fdcd55b43e4321806ab329e167846d7d42ae04f
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-12-01 05:19:21 -0800

    Task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,5 +5,6 @@ def print_last_digit(number):
         number = -(number)
 
     last_digit = number % 10
-
-    print(last_digit)
+    print("{:d}".format(last_digit), end='')
+    
+    return last_digit
