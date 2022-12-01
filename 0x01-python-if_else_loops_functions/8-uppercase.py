c64453649cd372caf6e09d30fc2f5d644508d4c1
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2022-11-30 22:14:46 -0800

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for char in str:
+        unicode_char = ord(char)
+        if unicode_char >= 97 and unicode_char <= 122:
+            unicode_char -= 32
+        print("{:c}".format(char), end="")
+    print()
