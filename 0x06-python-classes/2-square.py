1909970ac1c83667e8b7e9c7db5b76ab1672bdf3
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-01-30 12:30:10 -0800

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Created on Mon Jan 30 2023
+Author: Chibuokem Obiegbulem
+'''
+
+
+class Square:
+    '''
+    class Square that defines a square
+    '''
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
