37a833716e5a9afd5257d72310dafdbd4d5aa637
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-03-24 18:14:49 +0100

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+   script that lists all states from the database hbtn_0e_0_usa 
+'''
+import MySQLdb
+import sys
+
+
+if __name__ = '__main__':
+
+    username, password, database = sys.argv[1:]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
