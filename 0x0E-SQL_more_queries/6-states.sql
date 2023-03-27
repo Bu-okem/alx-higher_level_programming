9c494a556a2e17d95d5c8e2fde0beefcfc5c2df0
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-03-27 15:26:51 +0100

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,8 @@
 -- creates the database hbtn_0d_usa and the table states
-CREATE TABLE IF NOT EXISTS unique_id (
-	id INT DEFAULT 1 UNIQUE, 
-	name VARCHAR(256)
+-- create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
 );
