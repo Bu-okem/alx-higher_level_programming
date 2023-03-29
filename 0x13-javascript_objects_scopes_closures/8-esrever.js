7dab789f672fea199bdb56fc8505ca2cd8ad291a
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-03-29 10:42:35 +0100

    Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-
-  let reversed_list = [];
+  const reversedList = [];
   while (list.length > 0) {
-    reversed_list.push(list.pop());
+    reversedList.push(list.pop());
   }
-  return reversed_list;
+  return reversedList;
 };
