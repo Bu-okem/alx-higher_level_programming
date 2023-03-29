c5efa79ec45a7c68952b1833c8c6ed3e20afd154
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-03-29 09:34:54 +0100

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let n = 0;
+    while (n < this.height) {
+      console.log('X'.repeat(this.width));
+      n++;
+    }
+  }
+
+  rotate () {
+    const holder = this.width;
+    this.width = this.height;
+    this.height = holder;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
