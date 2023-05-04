30450430f834ba20143f2b3045460183e04b3320
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-05-04 02:19:04 +0100

    Task 9

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+// fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays
+// the value of hello from that fetch in the HTML tag DIV#hello
+
+$.getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+  $('#hello').text(data.hello);
+});
