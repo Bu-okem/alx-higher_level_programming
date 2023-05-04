96b9c09a63fc3dcbcb00766aa03a76e1166a2e65
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-05-04 02:14:41 +0100

    Task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,6 @@
 // fetches the character name from this
 // URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
 
-$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+$.getJSON('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
     $('DIV#character').text(data.name);
 });
