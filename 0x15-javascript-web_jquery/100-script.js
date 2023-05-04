150455cdebc947c5f037aead631124d87c2fc340
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-05-04 02:22:13 +0100

    Task 100

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// updates the text color of the <header> element to red (#FF0000)
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelector('header').style.color = '#FF0000';
+});
